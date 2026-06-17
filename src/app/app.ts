42adac11cbf02630904d335239a9abe959fffa53
Kayky <kaykyp.ribeiro@gmail.com>
2026-06-16 21:39:42 -0300

    criação do projeto e primeiras configurações

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Container } from './componentes/container/container';
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Separador } from './componentes/separador/separador';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Container, Cabecalho, Separador],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('indexa');
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
 }
