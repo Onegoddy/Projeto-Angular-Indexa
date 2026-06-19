2166e7be9786d8f52e656b7be3b4c23c328a3a99
Kayky <kaykyp.ribeiro@gmail.com>
2026-06-18 21:23:59 -0300

    finalização do projeto, adicionando json e um filtro para busca

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,44 @@ import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Separador } from './componentes/separador/separador';
 import { Contato } from './componentes/contato/contato';
 
+
+interface Contatoagenda{
+  id: number
+  nome: string
+  telefone: string
+}
+
+
+import agenda from './agenda.json';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container, Cabecalho, Separador,Contato],
+  imports: [RouterOutlet, Container, Cabecalho, Separador,Contato,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contatoagenda[] = agenda;
+
+  filtroportexto:string = ''
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosportexto(): Contatoagenda[] {
+    if (!this.filtroportexto){
+      return this.contatos
+    }
+    return this.contatos.filter(contato =>{
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroportexto).toLowerCase());
+    });
+  }
+
+  filtrarContatos(letra: string): Contatoagenda[] {
+    return this.filtrarContatosportexto().filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
+    });
+  }
 }
